218419ccd98ce527efe8570fa946845553a4ce2e
Filip Lindbladh <filip.lindbladh@beamonpeople.se>
2020-01-18 10:47:57 +0100

    update wp request path

--- src/apiKey.js
+++ src/apiKey.js
@@ -1,3 +1,3 @@
 export const apiKey = `dBvxjcQa9hPm36Re7uRpmWfnDzu4KhfD`;
-export const wpBaseUri = `https://172.105.90.47/wp`;
+export const wpBaseUri = `https://filil.se/wp/`;
 export const mixlrApi = `https://api.mixlr.com/users/5672861?source=embed&fbclid=IwAR3nPoKsh6mrQY7tqYHDAitocLs2xzW43gA9PJmVzmzAkax3gSo6JTtYe70`;
